b37eff181d29abb455c3bbf6f153dcc83d13751b
mat.p.wasilewski <mat.p.wasilewski@gmail.com>
2023-07-16 16:12:17 +0100

    add methods that check if output dir is empty

--- src/ensembl_ingest/ftp2local.py
+++ src/ensembl_ingest/ftp2local.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 from _socket import gaierror
 from ftplib import FTP
 from typing import Optional
@@ -7,13 +9,14 @@ from src.ensembl_ingest.utils.exceptions import FTPError
 
 class EnsemblFTPSession:
     def __init__(
-        self, organism: str, release: str, output_dir: Optional[str] = None
+            self, organism: str, release: str, output_dir: str = os.path.join(os.getcwd(), "ftp_output"), allow_overwrite: bool = False,
     ) -> None:
         self.organism_type = organism
         self.release = release
         self.format = "gff3"
         self.ensembl_url = "ftp.ensemblgenomes.ebi.ac.uk"
         self.output_dir = output_dir
+        self.allow_overwrite = allow_overwrite
 
         try:
             self._ftp = FTP(self.ensembl_url)
@@ -54,11 +57,27 @@ class EnsemblFTPSession:
         )
 
     def change_release_and_organism_type(
-        self, release: str, organism: str
+            self, release: str, organism: str
     ) -> None:
         self.change_release(release=release)
         self.change_organism_type(organism=organism)
 
-    # TODO: Add get to download files
-    def get(self, organism_name):
-        pass
+    def _confirm_organism_exists(self, organism_name: str):
+        organisms_list = {org.split("/")[-1] for org in self._ftp.nlst()}
+
+        if organism_name not in organisms_list:
+            raise FTPError(f"Organism: {organism_name} does not exist. Available organisms: {organisms_list}")
+
+    def get(self, organism_name: str):
+        self._confirm_organism_exists(organism_name)
+        self._confirm_output_dir()
+
+    def _confirm_output_dir(self):
+        if os.path.exists(self.output_dir):
+            if self.allow_overwrite:
+                shutil.rmtree(self.output_dir)
+            else:
+                raise FileExistsError(
+                    f"The directory '{self.output_dir}' already exists and 'allow_overwrite' is set to False.")
+        os.makedirs(self.output_dir)
+

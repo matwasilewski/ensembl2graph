16c098a9cd51296fb44ed407231c2e0da1a34a5d
mat.p.wasilewski <mat.p.wasilewski@gmail.com>
2023-07-16 15:00:00 +0100

    add test checking if gff3 present in ftp

--- test/test_ftp2local.py
+++ test/test_ftp2local.py
@@ -1,10 +1,19 @@
+import pytest
+
 from src.ensembl_ingest.ftp2local import EnsemblFTPSession
 
 
-def test_ensembl_session(caplog) -> None:
+@pytest.fixture()
+def session():
+    yield EnsemblFTPSession(organism="plants", release="release-55")
+
+
+def test_ensembl_session() -> None:
     # FIXME: This is acccesing the webserver directly. Should be replaced with https://pypi.org/project/pytest-localftpserver/
     session = EnsemblFTPSession(organism="plants", release="release-55")
     assert session._ftp.pwd() == "/pub/release-55/plants"
 
-    # list_in_plants = session.ftp.nlst()
-    # assert "gff3" in list_in_plants
+
+def test_if_gff3_present(session: EnsemblFTPSession) -> None:
+    list_in_plants = session._ftp.nlst()
+    assert "gff3" in list_in_plants

8b0a28e71054873c51e1f58b0dd35f28e7217b44
mat.p.wasilewski <mat.p.wasilewski@gmail.com>
2023-07-16 15:09:25 +0100

    lint with black, minor refactor

--- src/ensembl_ingest/ftp2local.py
+++ src/ensembl_ingest/ftp2local.py
@@ -6,7 +6,7 @@ from src.ensembl_ingest.utils.exceptions import FTPError
 
 class EnsemblFTPSession:
     def __init__(self, organism, release):
-        self.organism = None
+        self.organism_type = None
         self.release = None
         self.ensembl_url = "ftp.ensemblgenomes.ebi.ac.uk"
 
@@ -17,25 +17,34 @@ class EnsemblFTPSession:
             raise FTPError(
                 f"Failed to connect to {self.ensembl_url}. Error: {error}"
             )
-        self.change_release_and_organism(release=release, organism=organism)
+        self.change_release_and_organism_type(
+            release=release, organism=organism
+        )
 
     def change_release(self, release):
         releases = {r.split("/")[-1] for r in self._ftp.nlst("/pub")}
 
         if release not in releases:
-            raise FTPError(f"Release: {release} does not exist in {self.ensembl_url}. Available releases: {releases}/")
+            raise FTPError(
+                f"Release: {release} does not exist in {self.ensembl_url}. Available releases: {releases}/"
+            )
 
         self.release = release
 
-    def change_organism(self, organism):
-        available_organisms = {org.split("/")[-1] for org in self._ftp.nlst(f"/pub/{self.release}")}
+    def change_organism_type(self, organism):
+        available_organisms = {
+            org.split("/")[-1]
+            for org in self._ftp.nlst(f"/pub/{self.release}")
+        }
 
         if organism not in available_organisms:
-            raise FTPError(f"Organism: {organism} does not exist in {self.ensembl_url}, in release: {self.release}. Available organisms in this release: {available_organisms}")
+            raise FTPError(
+                f"Organism: {organism} does not exist in {self.ensembl_url}, in release: {self.release}. Available organisms in this release: {available_organisms}"
+            )
 
-        self.organism = organism
-        self._ftp.cwd(f"/pub/{self.release}/{self.organism}")
+        self.organism_type = organism
+        self._ftp.cwd(f"/pub/{self.release}/{self.organism_type}")
 
-    def change_release_and_organism(self, release, organism):
+    def change_release_and_organism_type(self, release, organism):
         self.change_release(release=release)
-        self.change_organism(organism=organism)
+        self.change_organism_type(organism=organism)

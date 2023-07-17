d2ca94070df3ba3b926f1cb20de5d6e863e504df
mat.p.wasilewski <mat.p.wasilewski@gmail.com>
2023-07-17 13:56:42 +0100

    add gff3 to json cli

--- src/ensembl_ingest/cli.py
+++ src/ensembl_ingest/cli.py
@@ -1,9 +1,11 @@
 import logging
 import os
+import sys
 
 import click
 
 from src.ensembl_ingest.ftp2local import EnsemblFTPSession
+from src.ensembl_ingest.gff3transform import GFF3Genome
 
 
 @click.command()
@@ -28,6 +30,10 @@ from src.ensembl_ingest.ftp2local import EnsemblFTPSession
     help='The output directory.',
 )
 def main(organism_name, organism_type, release, output_dir):
+    _download_files(organism_name, organism_type, release, output_dir)
+
+
+def _download_files(organism_name, organism_type, release, output_dir):
     logging.info(
         f"Starting session for organism_type: {organism_type}, release: {release} and output_dir: {output_dir}"
     )
@@ -36,6 +42,33 @@ def main(organism_name, organism_type, release, output_dir):
     )
     session.get(organism_name)
     session.close()
+
+
+@click.command()
+@click.option(
+    '--file', required=True, type=str, help='Path to the genome file'
+)
+@click.option(
+    '--output_file',
+    required=True,
+    help='The output JSON filename.',
+)
+def cli_to_graph(file_name, output_file_name):
+    if not output_file_name.endswith(".json"):
+        output_file_name += ".json"
+
+    if not os.path.isfile(file_name):
+        logging.error(f"Input file: {file_name} does not exist!")
+        sys.exit(1)
+
+    logging.info("Starting transform from gff3 to json...")
+    _gff3_to_graph_json(file_name, output_file_name)
+    logging.info("Transform from gff3 to json successful")
+
+
+def _gff3_to_graph_json(file_name, output_file_name):
+    genome = GFF3Genome(file_name)
+    genome.to_node_link_json(output_file_name)
 
 
 if __name__ == '__main__':

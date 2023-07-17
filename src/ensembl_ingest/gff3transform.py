97b946cca4d6a43e92e6e296c53e3cd8ef741069
mat.p.wasilewski <mat.p.wasilewski@gmail.com>
2023-07-17 11:12:27 +0100

    add loading from python's gff3 package

--- src/ensembl_ingest/gff3transform.py
+++ src/ensembl_ingest/gff3transform.py
@@ -1,9 +1,33 @@
+import gzip
 import os.path
+import shutil
+import tempfile
+
+from gff3 import Gff3
 
 from src.ensembl_ingest.utils.exceptions import GFF3Exception
 
 
 class GFF3Genome:
-    def __init__(self, path: str):
+    def __init__(self, path: str) -> None:
+        self.genome = Gff3()
+
         if not os.path.isfile(path):
             raise GFF3Exception(f"File: {path} does not exist!")
+
+        if path.endswith(".gz"):
+            self.unpack_genome_in_gz(path)
+        else:
+            self.genome.parse(path)
+
+    def unpack_genome_in_gz(self, path: str) -> None:
+        with tempfile.TemporaryDirectory() as temp_dir:
+            # Construct the path to the temporary file (in the temporary directory)
+            temp_file_path = os.path.join(temp_dir, 'temp_file')
+
+            # Open the .gz file, open the temporary file, and use `shutil.copyfileobj` to decompress the .gz file
+            with gzip.open(path, 'rb') as f_in:
+                with open(temp_file_path, 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+
+            self.genome.parse(temp_file_path)
